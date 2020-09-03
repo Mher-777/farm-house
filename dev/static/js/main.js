de216f33d7e37124829d61700a5b47f17a9c96dc
Mher-777 <mherjan2003@mail.ru>
2020-09-04 00:58:50 +0400

    end slider desktop version

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,5 +1,42 @@
 $(function () {
-    $('.hamburger').on('click', function () {
-        $(this).toggleClass('is-active')
-    })
+    const menuSidebar = () => {
+        $('.hamburger').on('click', function () {
+            $(this).toggleClass('is-active')
+        })
+    }
+    const progressBar = () => {
+        const progress = $('.info-user__progress')
+        const interest = $('.info-user__progress-content')
+        progress.each(function () {
+            const progressInterest = $(this).attr('data-percentage')
+            $(this).find(interest).animate({
+                'width': progressInterest
+            }, 2000)
+        })
+    }
+    const tabs = () => {
+        const link = $('.slider__footer-link')
+        const content = $('.slider__footer-content')
+        content.hide()
+        content.first().show()
+        link.on('click', function (e) {
+            e.preventDefault()
+            if(!$(this).parent().hasClass('slider__footer-item--current')) {
+                const linkId = Number($(this).attr('data-id'))
+                link.parent().removeClass('slider__footer-item--current')
+                $(this).parent().addClass('slider__footer-item--current')
+                content.each(function () {
+                    const contentId = Number($(this).attr('data-content-id'))
+                    if(linkId === contentId) {
+                        content.hide(600).removeClass('slider__footer-content--active')
+                        $(this).show(600).addClass('slider__footer-content--active')
+                    }
+                })
+            }
+        })
+
+    }
+    tabs()
+    progressBar()
+    menuSidebar()
 })
--- dist/static/js/main.js
+++ dist/static/js/main.js
@@ -1,7 +1,48 @@
 "use strict";
 
 $(function () {
-  $('.hamburger').on('click', function () {
-    $(this).toggleClass('is-active');
-  });
+  var menuSidebar = function menuSidebar() {
+    $('.hamburger').on('click', function () {
+      $(this).toggleClass('is-active');
+    });
+  };
+
+  var progressBar = function progressBar() {
+    var progress = $('.info-user__progress');
+    var interest = $('.info-user__progress-content');
+    progress.each(function () {
+      var progressInterest = $(this).attr('data-percentage');
+      $(this).find(interest).animate({
+        'width': progressInterest
+      }, 2000);
+    });
+  };
+
+  var tabs = function tabs() {
+    var link = $('.slider__footer-link');
+    var content = $('.slider__footer-content');
+    content.hide();
+    content.first().show();
+    link.on('click', function (e) {
+      e.preventDefault();
+
+      if (!$(this).parent().hasClass('slider__footer-item--current')) {
+        var linkId = Number($(this).attr('data-id'));
+        link.parent().removeClass('slider__footer-item--current');
+        $(this).parent().addClass('slider__footer-item--current');
+        content.each(function () {
+          var contentId = Number($(this).attr('data-content-id'));
+
+          if (linkId === contentId) {
+            content.hide(600).removeClass('slider__footer-content--active');
+            $(this).show(600).addClass('slider__footer-content--active');
+          }
+        });
+      }
+    });
+  };
+
+  tabs();
+  progressBar();
+  menuSidebar();
 });
